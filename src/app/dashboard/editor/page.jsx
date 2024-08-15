b962160380a3b5de69d82d9413edec8658b378b8
mshahid538 <m.shahid538@yahoo.com>
2024-08-15 16:33:44 +0500

    feat: added editor top bar settings and menu

--- src/app/dashboard/editor/page.jsx
+++ src/app/dashboard/editor/page.jsx
@@ -1,9 +1,21 @@
-import Editor from "@/components/layouts/editor";
+import EditorLeftPanel from "@/components/layouts/editorLeftPanel";
+import EditorRightPanel from "@/components/layouts/editorRightPanel";
+import EditorTopPanel from "@/components/layouts/editorTopPanel";
+import pdfSampleImage from "@/../public/images/pdf-example.png";
+import Image from "next/image";
 
 export default function page() {
   return (
     <main className="flex flex-col h-full overflow-hidden">
-      <Editor />
+      <EditorTopPanel />
+      <div className="relative flex flex-1 h-full overflow-auto">
+        <EditorLeftPanel />
+        <div className="flex-1 p-4 overflow-scroll">
+          <h3>Selected PDF document goes here</h3>
+          <Image className="w-full" src={pdfSampleImage} alt="" />
+        </div>
+        <EditorRightPanel />
+      </div>
     </main>
   );
 }
--- src/components/layouts/editorTopPanel.jsx
+++ src/components/layouts/editorTopPanel.jsx
@@ -1,12 +1,57 @@
+import {
+  ChevronDownIcon,
+  EyeIcon,
+  MessageSquareIcon,
+  Redo2Icon,
+  SettingsIcon,
+  Undo2Icon,
+  WrenchIcon,
+  ZoomInIcon,
+  ZoomOutIcon,
+} from "lucide-react";
+
 export default function EditorTopPanel() {
   return (
-    <section className="flex justify-between px-xs border-b-[1.5px] border-dark/30">
-      <span className="flex flex-col py-2 font-semibold">
+    <section className="px-xs flex items-center justify-between border-b-[1.5px] border-dark/30 bg-dark/5">
+      <div className="flex flex-col py-2 font-semibold">
         <p className="text-sm">Proxili Marketing doc 2034</p>
         <small className="text-xs text-dark/50">Price $150</small>
-      </span>
-      <span></span>
-      <span></span>
+      </div>
+      <div className="gap-sm flex">
+        <span className="gap-xs flex items-center">
+          <ZoomOutIcon className="icon-btn" />
+          <small>100%</small>
+          <ZoomInIcon className="icon-btn" />
+        </span>
+        <span className="gap-xs flex items-center">
+          <Undo2Icon className="icon-btn" />
+          <Redo2Icon className="icon-btn" />
+        </span>
+      </div>
+      <Menu />
     </section>
   );
 }
+
+const Menu = () => {
+  const menuItems = [
+    { name: "Preview", icon: <EyeIcon className="icon-sm" /> },
+    { name: "Comment", icon: <MessageSquareIcon className="icon-sm" /> },
+    { name: "Settings", icon: <SettingsIcon className="icon-sm" /> },
+    { name: "Tools", icon: <WrenchIcon className="icon-sm" /> },
+  ];
+  return (
+    <nav className="gap-xs flex">
+      {menuItems.map((item, i) => (
+        <span className="px-3 flex gap-2 py-2 hover:bg-light/50 rounded-md cursor-pointer" key={i}>
+          {item.icon}
+          <small>{item.name}</small>
+        </span>
+      ))}
+      <span className="px-3 flex gap-2 py-2 hover:bg-light/50 rounded-md cursor-pointer">
+        <ChevronDownIcon className="icon-sm" />
+        <small>More</small>
+      </span>
+    </nav>
+  );
+};
